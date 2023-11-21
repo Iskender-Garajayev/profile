71f44c2be4095d4f79079f37274593e9cf14fdc3
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-11-21 11:19:18 -0800

    7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AboutMe from './components/AboutMe';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <AboutMe/>
     </div>
   );
 }
