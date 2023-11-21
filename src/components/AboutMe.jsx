7bd574286a542b2b146cade7f6b0d7e45d52f0db
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-11-22 00:23:10 +0300

    2

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import Avatar from '../img/avataaars.png'
+import Avatar from '../img/avataaars.png';
 
 const AboutMe = () => {
-  return <div className='about-me-container' id='aboutme'>
-    <img src={Avatar} alt="" />
-  </div>;
+  return (
+    <div className='about-container' id='aboutme'>
+      <div className='about-inner'>
+        <div className='about-title'>
+          <p>Hi, my name is Iskender Garajayev</p>
+        </div>
+        <div className='about-img'>
+          <img src={Avatar} alt='' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
