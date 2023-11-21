26ea436c02be668c8f1a84e178c4003b620f915b
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-11-21 08:22:47 +0300

    3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
-    <div>
-      İskender Garajayev
+    <div className='header-container'>
+      <div className='header-inner'>
+        <div className='logo'>I.G</div>
+        <div className='navbar'>
+          <ul>
+            <li>about me</li>
+            <li>skils</li>
+            <li>projects</li>
+            <li></li>
+            <li>contacts</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
