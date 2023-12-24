c13dc55ee9010460b238ea4c910d16b3ed0751d8
Iskender Garajayev <yoloten.isa@gmail.com>
2023-12-24 10:36:29 -0800

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -168,10 +168,18 @@ const Header = () => {
             <svg
               onClick={toggleClass}
               xmlns='http://www.w3.org/2000/svg'
-              height='1.3em'
-              viewBox='0 0 448 512'
+              class='ionicon'
+              height='2em'
+              viewBox='0 0 512 512'
             >
-              <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
+              <path
+                fill='none'
+                stroke='currentColor'
+                stroke-linecap='round'
+                stroke-miterlimit='10'
+                stroke-width='32'
+                d='M80 160h352M80 256h352M80 352h352'
+              />
             </svg>
           </div>
           <div className={isActive ? 'close-icon' : 'active-close'}>
