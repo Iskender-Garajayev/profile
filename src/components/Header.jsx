f5cb26f4a8f818240c6cec5aba5aa2d5436711bd
Iskender Garajayev <yoloten.isa@gmail.com>
2023-12-24 12:22:49 -0800

    12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,6 +25,7 @@ const Header = () => {
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 class='ionicon'
+                height='2em'
                 viewBox='0 0 512 512'
               >
                 <path
@@ -43,6 +44,7 @@ const Header = () => {
                   stroke-width='32'
                 />
               </svg>
+              {/* <p>about me</p> */}
             </li>
             <li>
               <svg
@@ -184,12 +186,20 @@ const Header = () => {
           </div>
           <div className={isActive ? 'close-icon' : 'active-close'}>
             <svg
-              onClick={toggleClass}
               xmlns='http://www.w3.org/2000/svg'
-              height='1em'
-              viewBox='0 0 384 512'
+              class='ionicon'
+              height='2em'
+              onClick={toggleClass}
+              viewBox='0 0 512 512'
             >
-              <path d='M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z' />
+              <path
+                fill='none'
+                stroke='currentColor'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                stroke-width='32'
+                d='M368 368L144 144M368 144L144 368'
+              />
             </svg>
           </div>
         </div>
