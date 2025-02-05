59b37a6725c4983fcd4dc7e0b0f38d78508d4154
Iskender Garajayev <yoloten.isa@gmail.com>
2025-02-05 20:19:45 +0500

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/projects.css';
 import { Link } from 'react-router-dom';
 import Youtube from '../img/YoutubeLone.jpg'
+import Ecommerce from '../img/e-commerce.png'
 
 
 const Projects = () => {
@@ -17,53 +18,24 @@ const Projects = () => {
 
           <div className='item-title'></div>
         </div>
+
+
         <div className='item-box'>
-          <div className='item-img'></div>
+          <div className='item-img'>
+            <Link className='link' target="_blank" to='https://shop-app-i-g.vercel.app/'>
+            <img src={Ecommerce} alt="Youtube Clone" />
+            </Link>
+          </div>
 
           <div className='item-title'></div>
         </div>
-        <div className='item-box'>
-          <div className='item-img'></div>
 
-          <div className='item-title'></div>
-        </div>
-        <div className='item-box'>
-          <div className='item-img'></div>
 
-          <div className='item-title'></div>
-        </div>
-        <div className='item-box'>
-          <div className='item-img'></div>
+        
 
-          <div className='item-title'></div>
-        </div>
-        <div className='item-box'>
-          <div className='item-img'></div>
-
-          <div className='item-title'></div>
-        </div>
-        <div className='item-box'>
-          <div className='item-img'></div>
-
-          <div className='item-title'></div>
-        </div>
-        <div className='item-box'>
-          <div className='item-img'></div>
-
-          <div className='item-title'></div>
-        </div>
-        <div className='item-box'>
-          <div className='item-img'></div>
-
-          <div className='item-title'></div>
-        </div>
-        <div className='item-box'>
-          <div className='item-img'></div>
-
-          <div className='item-title'></div>
         </div>
       </div>
-    </div>
+    
   );
 };
 
